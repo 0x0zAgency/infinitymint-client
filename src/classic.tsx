d31395fcb4cdb31c5d1e5be782ebfcfd852a57cb
Llydia Cross <llydiacross@gmail.com>
2023-02-19 00:20:27 +0000

    f

---
+++ src/classic.tsx
@@ -0,0 +1,25 @@
+import * as Config from './classic/config';
+import * as Controller from './classic/controller';
+import * as ModController from './classic/modController';
+import * as StorageController from './classic/storageController';
+import * as Helpers from './classic/helpers';
+import * as PageController from './classic/pageController';
+import * as StickerController from './classic/stickerController';
+import * as Resources from './classic/resources';
+import * as TinySVG from './classic/tinysvg';
+import * as IPFSController from './classic/ipfsController';
+import * as TokenMethods from './classic/tokenMethods';
+
+export {
+    Config,
+    Controller,
+    ModController,
+    StorageController,
+    Helpers,
+    PageController,
+    StickerController,
+    Resources,
+    TinySVG,
+    IPFSController,
+    TokenMethods,
+};

1d7ab57c2f947ab16f7dd0bc36519c7b8f617ee8
koma <m@kae.si>
2023-02-21 23:07:37 +0000

    Feat: Custom testing suite runner

--- dev/test.ts
+++ dev/test.ts
@@ -1,2 +1,34 @@
-import mocha from 'mocha';
 import controller from '../src/classic/controller';
+import * as ReactConfig from '../src/classic/utils/config';
+import glob from 'glob';
+
+(async () => {
+    try {
+        // Load the default projectUI just for testing
+        controller.defaultProjectURI = await controller.getProjectURI(
+            'default'
+        );
+
+        // Add all the files to mocha.
+        await new Promise((resolve, reject) => {
+            // Relative to the project root.
+            glob('/tests/**/*.test.ts', (error, files) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                resolve(files.map((filePath) => mocha.addFile(filePath)));
+            });
+        });
+
+        // After grabbing the files, start the server
+        await controller.start(ReactConfig);
+        console.log('React server has started!');
+        // Now that the server is up, run our test suite!
+        mocha.run();
+    } catch (error) {
+        console.error(error.stack);
+        process.exit(1);
+    }
+})();

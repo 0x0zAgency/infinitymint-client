26332f22c5df3932c58cdd321a86ff0394cbf8ca
koma <m@kae.si>
2023-02-21 22:32:56 +0000

    Fix: Import config

--- tests/classic/controller.test.ts
+++ tests/classic/controller.test.ts
@@ -1,9 +1,9 @@
-import controller from '../../src/classic/controller';
-import { expect } from 'chai';
 import 'mocha';
+import controller from '../../src/classic/controller';
+import config from '../../src/classic/utils/config';
 
 describe('[Classic] Controller Class', () => {
-    it('Executes a contract on chain using `#callMethod`', () => {
+    it('Executes a contract on chain using `#callMethod`', async () => {
         controller.sendMethod(
             controller.accounts[0],
             'InfinityMint',
